236ded72f5a06d8a9dbef6ff18aaa578d418215c
Brian Peters <BrianRPeters@gmail.com>
2023-10-02 21:15:00 -0400

    Update le-search.py

--- le-search.py
+++ le-search.py
@@ -5,18 +5,27 @@ import ctypes
 import subprocess
 import csv
 
+
 def check_tesseract_installed():
     try:
-        subprocess.run(["tesseract", "--version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run(
+            ["tesseract", "--version"],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
     except (subprocess.CalledProcessError, FileNotFoundError):
         print("Tesseract not found. Please install it to proceed.")
         exit(1)
 
+
 def get_clipboard_image():
     return ImageGrab.grabclipboard()
 
+
 def ocr_image(image):
     return pytesseract.image_to_string(image)
+
 
 def filter_capitalized_words(text):
     words = text.split()
@@ -25,28 +34,29 @@ def filter_capitalized_words(text):
     # Remove single-letter words from the beginning
     while capitalized_words and len(capitalized_words[0]) == 1:
         capitalized_words.pop(0)
+    return " ".join(capitalized_words)
 
-    return ' '.join(capitalized_words)
 
 def get_reroll_chance(item_name):
-    with open('items.db', mode ='r') as file:
+    with open("items.db", mode="r") as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:
             if row[0].upper() == item_name.upper():
                 return row[1]
     return None
 
+
 def show_dialog(text):
-    #print("RESULT:")
-    #print(text)
+    # print("RESULT:")
+    # print(text)
     ctypes.windll.user32.MessageBoxW(0, text, "LE Search Result", 0x1000)
+
 
 def main():
     image = get_clipboard_image()
     if image is None:
         show_dialog("No image found on clipboard.")
         return
-
     text = ocr_image(image)
 
     if text:
@@ -60,15 +70,15 @@ def main():
                 reroll_text = "No weight assigned"
             else:
                 reroll_text = f"Reroll Chance: {reroll_chance}"
-
             show_dialog(f"{filtered_text}\n{reroll_text}")
         else:
             show_dialog(f"{filtered_text}\nItem not found in database.")
     else:
         show_dialog("No text found.")
 
+
 # Check if Tesseract is installed
 check_tesseract_installed()
 
-keyboard.add_hotkey('ctrl+shift+d', main)
+keyboard.add_hotkey("ctrl+shift+d", main)
 keyboard.wait()
