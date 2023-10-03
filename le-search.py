681d97f106292ed7c5caa85549b5118755450fc4
Brian Peters <BrianRPeters@gmail.com>
2023-10-02 21:36:56 -0400

    Update le-search.py
    docstrings

--- le-search.py
+++ le-search.py
@@ -7,6 +7,9 @@ import csv
 
 
 def check_tesseract_installed():
+    """
+    Checks if Tesseract is installed on the system.
+    """
     try:
         subprocess.run(
             ["tesseract", "--version"],
@@ -20,14 +23,23 @@ def check_tesseract_installed():
 
 
 def get_clipboard_image():
+    """
+    Grabs the image from the clipboard.
+    """
     return ImageGrab.grabclipboard()
 
 
 def ocr_image(image):
+    """
+    Performs OCR on the given image.
+    """
     return pytesseract.image_to_string(image)
 
 
 def filter_capitalized_words(text):
+    """
+    Filters out capitalized words from the given text.
+    """
     words = text.split()
     capitalized_words = [word for word in words if word.isupper()]
 
@@ -38,6 +50,9 @@ def filter_capitalized_words(text):
 
 
 def get_reroll_chance(item_name):
+    """
+    Retrieves the reroll chance for the given item name from the database.
+    """
     with open("items.db", mode="r") as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:
@@ -47,12 +62,16 @@ def get_reroll_chance(item_name):
 
 
 def show_dialog(text):
-    # print("RESULT:")
-    # print(text)
+    """
+    Displays a dialog box with the given text.
+    """
     ctypes.windll.user32.MessageBoxW(0, text, "LE Search Result", 0x1000)
 
 
 def main():
+    """
+    Main function to execute the OCR and display the result.
+    """
     image = get_clipboard_image()
     if image is None:
         show_dialog("No image found on clipboard.")
